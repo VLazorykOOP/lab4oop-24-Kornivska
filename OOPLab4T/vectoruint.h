f258263167827b890273d9bcd5a14ad17f7ba249
Аліна Корнівська <alinkornivska@gmail.com>
2024-04-21 23:54:07 +0300

    перезавантаження операторів

--- OOPLab4T/vectoruint.h
+++ OOPLab4T/vectoruint.h
@@ -16,3 +16,26 @@ public: // Початок публічної секції класу
 	VectorUInt(unsigned int elems, unsigned int value); // Конструктор з параметрами - створення вектору заданого розміру та заповнення його значенням
 	VectorUInt(VectorUInt& inp); // Конструктор копіювання
 	~VectorUInt(); // Деструктор#pragma once
+	VectorUInt operator++(int); // Перегрузка оператора постфіксного інкремента
+	VectorUInt operator--(int); // Перегрузка оператора постфіксного декремента
+	bool operator!(); // Перегрузка оператора логічного заперечення
+	VectorUInt operator~(); // Перегрузка оператора бітового заперечення
+	VectorUInt operator-(); // Перегрузка оператора від'ємного числа
+	VectorUInt operator=(VectorUInt& input); // Перегрузка оператора присвоєння
+	VectorUInt operator+(VectorUInt& input); // Перегрузка оператора додавання векторів
+	VectorUInt operator+(unsigned int input); // Перегрузка оператора додавання вектора та беззнакового цілого числа
+
+	// Додаткові перегрузки операторів арифметичних операцій, бітових операцій та операцій присвоєння
+
+	friend std::ostream& operator<<(std::ostream& os, const VectorUInt& out); // Дружня функція для виводу вектора в потік
+	friend std::istream& operator>>(std::istream& is, const VectorUInt& in); // Дружня функція для зчитування вектора з потоку вводу
+
+	// Додаткові перегрузки операторів порівняння, індексування та виклику
+
+	bool operator>(VectorUInt& input); // Перевантаження оператора "більше"
+	bool operator>=(VectorUInt& input); // Перевантаження оператора "більше або дорівнює"
+	bool operator<(VectorUInt& input); // Перевантаження оператора "менше"
+	bool operator<=(VectorUInt& input); // Перевантаження оператора "менше або дорівнює"
+};
+
+#endif // Завершення директиви умовного компілятора: VECTORUINT_H
