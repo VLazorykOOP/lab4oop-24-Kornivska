c6f2f911c7731a2fdccd8a47f920798a5c2ae9aa
Аліна Корнівська <alinkornivska@gmail.com>
2024-04-26 12:39:08 +0300

    ееее

--- OOPLab4T/main.cpp
+++ OOPLab4T/main.cpp
@@ -1,125 +1,329 @@
-#include "vectoruint.h" // Включення заголовного файлу з описом класу VectorUInt
 #include <iostream>
+#include <stdexcept> // Для виключень
+#include <unordered_map> // Для використання unordered_map
+#include <string>
 
-using namespace std;
+// Клас для роботи з беззнаковими цілими векторами
+class VectorUInt {
+private:
+    unsigned int* data; // Вказівник на масив беззнакових цілих чисел
+    size_t size; // Кількість елементів у векторі
+    int codeError; // Код помилки
+    
+public:
+    // Конструктори
 
-int main() {
-	VectorUInt A(5, 2); // Створення вектора A розміром 5 та ініціалізація всіх його елементів значенням 2
-	VectorUInt B(3); // Створення вектора B розміром 3 зі значеннями за замовчуванням
-	VectorUInt C; // Створення порожнього вектора C
+    // Конструктор без параметрів
+    VectorUInt() {
+        size = 1;
+        data = new unsigned int[size];
+        data[0] = 0;
+        codeError = 0;
+    }
 
-	// Виведення векторів A, B та C на екран
-	cout << "A: " << A << endl;
-	cout << "B: " << B << endl;
-	cout << "C: " << C << endl;
+    // Конструктор з одним параметром - розмір вектора
+    VectorUInt(size_t s) {
+        size = s;
+        data = new unsigned int[size];
+        for (size_t i = 0; i < size; ++i) {
+            data[i] = 0;
+        }
+        codeError = 0;
+    }
 
-	// Виведення кількості створених об'єктів VectorUInt
-	cout << "Об'єкти: " << A.count << endl;
+    // Конструктор з двома параметрами - розмір вектора та значення ініціалізації
+    VectorUInt(size_t s, unsigned int value) {
+        size = s;
+        data = new unsigned int[size];
+        for (size_t i = 0; i < size; ++i) {
+            data[i] = value;
+        }
+        codeError = 0;
+    }
 
-	// Створення копії вектора A за допомогою конструктора копіювання
-	VectorUInt D(A);
+    // Конструктор копіювання
+    VectorUInt(const VectorUInt& other) {
+        size = other.size;
+        data = new unsigned int[size];
+        for (size_t i = 0; i < size; ++i) {
+            data[i] = other.data[i];
+        }
+        codeError = other.codeError;
+    }
 
-	// Виведення вектора D на екран
-	cout << "D: " << D << endl;
+    // Деструктор
+    ~VectorUInt() {
+        delete[] data;
+    }
 
-	// Виведення кількості об'єктів VectorUInt після створення D
-	cout << "Об'єкти: " << A.count << endl;
-	// Інкрементування та декрементування вектора D за допомогою унарних операторів
-	D++;
-	cout << "D: " << D << endl;
-	D--;
-	cout << "D: " << D << endl;
+    // Перевантаження операторів
 
-	// Логічне заперечення порожнього вектора С
-	cout << "!C: " << !C << endl;
+    // Унарні префіксні та постфіксні ++ та --
+    VectorUInt& operator++() {
+        for (size_t i = 0; i < size; ++i) {
+            ++data[i];
+        }
+        return *this;
+    }
 
-	// Побітове доповнення елементів вектора A
-	cout << "~A: " << ~A << endl;
-	cout << "~A: " << ~A << endl;
+    VectorUInt operator++(int) {
+        VectorUInt temp(*this);
+        operator++();
+        return temp;
+    }
 
-	// Виведення вектора A на екран
-	cout << "A: " << A << endl;
+    VectorUInt& operator--() {
+        for (size_t i = 0; i < size; ++i) {
+            --data[i];
+        }
+        return *this;
+    }
 
-	// Виведення протилежного вектора A
-	cout << "-A: " << -A << endl;
-	cout << "-A: " << -A << endl;
-	// Виконання арифметичних операцій між векторами та константами
-	cout << "A+D: " << A + D << endl;
-	cout << "A-D: " << A - D << endl;
-	cout << "A+1: " << A + 1 << endl;
-	cout << "A-1: " << A - 1 << endl;
-	cout << "A*D: " << A * D << endl;
-	cout << "A*2: " << A * 2 << endl;
-	cout << "A/D: " << A / D << endl;
-	cout << "A/2: " << A / 2 << endl;
-	cout << "A%2: " << A % 2 << endl;
-	cout << "A|D: " << (A | D) << endl;
-	cout << "A|3: " << (A | 3) << endl;
-	cout << "A^D: " << (A ^ D) << endl;
-	cout << "A^3: " << (A ^ 3) << endl;
+    VectorUInt operator--(int) {
+        VectorUInt temp(*this);
+        operator--();
+        return temp;
+    }
 
-	// Виконання операцій присвоєння з об'єктами та константами
-	A += D;
-	cout << "A+=D: " << A << endl;
-	A -= D;
-	cout << "A-=D: " << A << endl;
-	A += 3;
-	cout << "A+=3: " << A << endl;
-	A -= 3;
-	cout << "A-=3: " << A << endl;
-	A *= D;
-	cout << "A*=D: " << A << endl;
-	A /= D;
-	cout << "A/=D: " << A << endl;
-	A *= 3;
-	cout << "A*=3: " << A << endl;
-	A /= 3;
-	cout << "A/=3: " << A << endl;
+    // Унарна логічна !
+    bool operator!() {
+        return size != 0;
+    }
 
-	// Створення вектора L, що є копією вектора A
-	VectorUInt L(A);
-	cout << "L: " << L << endl;
+    // Унарна побітова ~
+    VectorUInt operator~() {
+        VectorUInt result(*this);
+        for (size_t i = 0; i < size; ++i) {
+            result.data[i] = ~result.data[i];
+        }
+        return result;
+    }
 
-	// Виконання побітової операції модуля з числом 2
-	L %= 2;
-	cout << "L%=2: " << L << endl;
+    // Унарний арифметичний -
+    VectorUInt operator-() {
+        VectorUInt result(*this);
+        for (size_t i = 0; i < size; ++i) {
+            int signedValue = static_cast<int>(result.data[i]);
+            signedValue = -signedValue;
+            result.data[i] = static_cast<unsigned int>(signedValue);
+        }
+        return result;
+    }
 
-	// Виконання побітової операції додавання з числом 20
-	L |= 20;
-	cout << "L|=20: " << L << endl;
+    // Оператор присвоєння =
+    VectorUInt& operator=(const VectorUInt& other) {
+        if (this != &other) {
+            delete[] data;
+            size = other.size;
+            data = new unsigned int[size];
+            for (size_t i = 0; i < size; ++i) {
+                data[i] = other.data[i];
+            }
+            codeError = other.codeError;
+        }
+        return *this;
+    }
 
-	// Виконання побітової операції виключаючого "або" з числом 10
-	L ^= 10;
-	cout << "L^10: " << L << endl;
+    // Оператори +=, -=, *=, /=, %=, |=, ^=, &= (бінарні)
+    VectorUInt& operator+=(const VectorUInt& other) {
+        if (size != other.size) {
+            throw std::invalid_argument("Vectors must be of the same size for addition");
+        }
+        for (size_t i = 0; i < size; ++i) {
+            data[i] += other.data[i];
+        }
+        return *this;
+    }
 
-	// Створення копії вектора A
-	VectorUInt copyA(A);
+    VectorUInt& operator-=(const VectorUInt& other) {
+        if (size != other.size) {
+            throw std::invalid_argument("Vectors must be of the same size for subtraction");
+        }
+        for (size_t i = 0; i < size; ++i) {
+            data[i] -= other.data[i];
+        }
+        return *this;
+    }
 
-	// Виведення кількості об'єктів VectorUInt
-	cout << "Об'єкти: " << A.count << endl;
+    // Арифметичні бінарні оператори +, -, *, /, %
+    VectorUInt operator+(const VectorUInt& other) const {
+        if (size != other.size) {
+            throw std::invalid_argument("Vectors must be of the same size for addition");
+        }
+        VectorUInt result(*this);
+        result += other;
+        return result;
+    }
 
-	// Порівняння векторів A та copyA на рівність та нерівність
-	cout << "A==copyA: " << (A == copyA) << endl;
-	cout << "A!=copyA: " << (A != copyA) << endl;
+    // Оператор порівняння ==
+    bool operator==(const VectorUInt& other) const {
+        if (size != other.size) {
+            return false;
+        }
+        for (size_t i = 0; i < size; ++i) {
+            if (data[i] != other.data[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	// Виведення значення елементу вектора A за індексом 2
-	cout << "A[2]: " << A[2] << endl;
+    // Оператор індексації []
+    unsigned int& operator[](size_t index) {
+        if (index >= size) {
+            codeError = 1;
+            index = size - 1;
+        }
+        return data[index];
+    }
 
-	// Створення двох векторів Z та X
-	VectorUInt Z(3, 4);
-	VectorUInt X(3, 5);
+    // Дружні функції для вводу та виводу
+    friend std::ostream& operator<<(std::ostream& os, const VectorUInt& vec);
+    friend std::istream& operator>>(std::istream& is, VectorUInt& vec);
+};
 
-	// Виведення векторів Z та X на екран
-	cout << "Z: " << Z << endl;
-	cout << "X: " << X << endl;
-
-	// Порівняння векторів Z та X на більше, менше, більше або дорівнює та менше або дорівнює
-	cout << "Z > X: " << (Z > X) << endl;
-	cout << "Z < X: " << (Z < X) << endl;
-	cout << "Z >= X: " << (Z >= X) << endl;
-	cout << "Z <= X: " << (Z <= X) << endl;
-
-	return 0;
+// Функції для виводу та вводу
+std::ostream& operator<<(std::ostream& os, const VectorUInt& vec) {
+    os << "[ ";
+    for (size_t i = 0; i < vec.size; ++i) {
+        os << vec.data[i] << " ";
+    }
+    os << "]";
+    return os;
 }
 
-	
+std::istream& operator>>(std::istream& is, VectorUInt& vec) {
+    for (size_t i = 0; i < vec.size; ++i) {
+        is >> vec.data[i];
+    }
+    return is;
+}
+
+// Клас для асоціативної сутності
+class AssociativeEntity {
+private:
+    std::unordered_map<std::string, std::string> associations;
+    int codeError;
+
+public:
+    // Конструктор за замовчуванням
+    AssociativeEntity() : codeError(0) {}
+
+    // Додавання асоціативної сутності
+    void addAssociation(const std::string& entity1, const std::string& entity2) {
+        associations[entity1] = entity2;
+    }
+
+    // Перевантаження оператора індексації []
+    std::string& operator[](const std::string& entity) {
+        if (associations.find(entity) == associations.end()) {
+            codeError = 1;
+            static std::string emptyString;
+            return emptyString;
+        }
+        return associations[entity];
+    }
+
+    // Функція-оператор виклику ()
+    std::string& operator()(const std::string& entity) {
+        return (*this)[entity];
+    }
+
+    // Дружні функції для виводу та вводу
+    friend std::istream& operator>>(std::istream& is, AssociativeEntity& ae);
+    friend std::ostream& operator<<(std::ostream& os, const AssociativeEntity& ae);
+};
+
+// Функції для виводу та вводу
+std::istream& operator>>(std::istream& is, AssociativeEntity& ae) {
+    std::string entity1, entity2;
+    std::cout << "Enter entity1: ";
+    is >> entity1;
+    std::cout << "Enter entity2: ";
+    is >> entity2;
+    ae.addAssociation(entity1, entity2);
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const AssociativeEntity& ae) {
+    for (const auto& association : ae.associations) {
+        os << association.first << " -> " << association.second << std::endl;
+    }
+    return os;
+}
+
+// Головна функція
+int main() {
+    int choice;
+
+    // Виведення меню та вибір операції
+    std::cout << "Choose an operation:\n"
+        << "1. Vector Operations\n"
+        << "2. Associative Entity\n";
+    std::cout << "Enter your choice: ";
+    std::cin >> choice;
+
+    switch (choice) {
+    case 1: {
+        std::cout << "Vector Operations\n";
+
+        // Виклик функцій для векторів
+        VectorUInt vec1;
+        std::cout << "vec1: " << vec1 << std::endl;
+
+        VectorUInt vec2(5);
+        std::cout << "vec2: " << vec2 << std::endl;
+
+        VectorUInt vec3(3, 10);
+        std::cout << "vec3: " << vec3 << std::endl;
+
+        VectorUInt vec4 = vec3;
+        std::cout << "vec4: " << vec4 << std::endl;
+
+        ++vec3;
+        std::cout << "vec3 after ++: " << vec3 << std::endl;
+
+        --vec4;
+        std::cout << "vec4 after --: " << vec4 << std::endl;
+
+        std::cout << "!vec3: " << !vec3 << std::endl;
+
+        VectorUInt vec5 = ~vec3;
+        std::cout << "vec5 after ~: " << vec5 << std::endl;
+
+        VectorUInt vec6 = -vec3;
+        std::cout << "vec6 after -: " << vec6 << std::endl;
+
+        vec1 = vec3;
+        std::cout << "vec1 after =: " << vec1 << std::endl;
+
+        vec1 += vec3;
+        std::cout << "vec1 after +=: " << vec1 << std::endl;
+
+        VectorUInt vec7 = vec3 + vec4;
+        std::cout << "vec7 after +: " << vec7 << std::endl;
+        break;
+    }
+    case 2: {
+        std::cout << "Associative Entity\n";
+
+        // Виклик функцій для асоціативної сутності
+        AssociativeEntity ae;
+
+        ae.addAssociation("FlightNumber", "DepartureTime");
+        ae.addAssociation("BusNumber", "DepartureTime");
+
+        std::cout << "Associations:\n" << ae << std::endl;
+
+        std::cout << "Departure time for FlightNumber: " << ae["FlightNumber"] << std::endl;
+
+        std::cout << "Departure time for BusNumber: " << ae("BusNumber") << std::endl;
+        break;
+    }
+    default:
+        std::cout << "Invalid choice!\n";
+        break;
+    }
+
+    return 0;
+}
